659af128a53be0625e9968038d2d70562e1c4c30
Julia <imelnych@e2r2p4.unit.ua>
2018-05-07 19:35:41 +0300

    gulp structure

---
+++ index.php
@@ -0,0 +1,30 @@
+<!-- <?php
+?> -->
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Camagru</title>
+		<link rel="stylesheet" href="pro/mincss/style.min.css">
+	</head>
+
+	<body>
+		<div class="container">
+			<form id="login-form">
+				<div class="row">
+					<div class="col-sm-6 col-sm-offset-3">
+						<div class="form-group">
+							<label>Email</label>
+							<input type="text" class="form-control" name="email">
+						</div>
+					<div class="form-group">
+							<label>Password</label>
+							<input type="password" class="form-control" name="password">
+					</div>
+				<button type="submit" class="btn btn-primary">Log In</button>
+			  </div>
+			</div>
+		  </form>
+		</div>
+	</body>
+</html>
